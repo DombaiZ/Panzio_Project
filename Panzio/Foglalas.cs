59242a865a7b7ae8dafe79568e9a7b49f34504e6
dombaizoltan <dombaiz@kkszki.hu>
2024-05-21 11:38:29 +0200

    Add project files.

---
+++ Panzio/Foglalas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panzio
+{
+    public class Foglalas
+    {
+        int szobaszam;
+        int ferohely;
+        int ar;
+
+        public Foglalas(int szobaszam, int ferohely, int ar)
+        {
+            this.Szobaszam = szobaszam;
+            this.Ferohely = ferohely;
+            this.Ar = ar;
+        }
+
+        public int Szobaszam { get => szobaszam; set => szobaszam = value; }
+        public int Ferohely { get => ferohely; set => ferohely = value; }
+        public int Ar { get => ar; set => ar = value; }
+    }
+}
---
+++ Panzio/Szallas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panzio
+{
+    internal class Szallas
+    {
+        int ugyfelID;
+        string ugyfelNev;
+        TimeSpan szulEv;
+        string email;
+        bool ugyfelVIP;
+
+        public Szallas(int ugyfelID, string ugyfelNev, TimeSpan szulEv, string email, bool ugyfelVIP)
+        {
+            this.UgyfelID = ugyfelID;
+            this.UgyfelNev = ugyfelNev;
+            this.SzulEv = szulEv;
+            this.Email = email;
+            this.UgyfelVIP = ugyfelVIP;
+        }
+
+        public override string ToString()
+        {
+            return $"{ugyfelID};{ugyfelNev},{szulEv},{email},{ugyfelVIP}\n";
+        }
+
+        public int UgyfelID { get => ugyfelID; set => ugyfelID = value; }
+        public string UgyfelNev { get => ugyfelNev; set => ugyfelNev = value; }
+        public TimeSpan SzulEv { get => szulEv; set => szulEv = value; }
+        public string Email { get => email; set => email = value; }
+        public bool UgyfelVIP { get => ugyfelVIP; set => ugyfelVIP = value; }
+    }
+}
